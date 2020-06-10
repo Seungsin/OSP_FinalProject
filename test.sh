97f2f6a79c346c70eee5f5166cd26596b6190a32
Seungsin <62578738+Seungsin@users.noreply.github.com>
2020-06-10 18:57:15 +0900

    shell file(not complete)
    just for testing

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir OSP_Final
+cd OSP_Final
+mkdir templates
+mkdir uploads
+cd
+cd OSP_FinalProject
+mv first_python.py ../OSP_Final/
+mv HTMLPage1.html ../OSP_Final/templates/
+cd ../OSP_Final/
+chmod 755 first_python.py
+export FLASK_APP=first_python.py
+./first_python.py
